5933cc320e9c1a85e4cdc09114a4146121bf0afd
Flávia Reis <flaviarster@gmail.com>
2024-04-18 16:39:13 -0300

    Treinando o comando de saída printf

--- printf.c
+++ printf.c
@@ -5,9 +5,9 @@ int main() {
 	
 	printf("Valor inteiro: %d.\n", 10);
 	printf("Valor decimal: %f.\n", 3.141516171819);
-	printf("Valor decimal com apenas duas casas após a vírgula: %.2f.\n", 3.141516171819);
-	printf("Imprimindo caractere: %c.\n", 'a');
-	printf("Imprimindo texto: %s.\n", "testando");
+	printf("Valor decimal com apenas duas casas após a vírgula: %.2f.\n", 3.141516171819); // Coloca o número de casas decimais após %.
+	printf("Imprimindo caractere: %c.\n", 'a'); // Um único caractere é cercado por aspas simples
+	printf("Imprimindo texto: %s.\n", "testando"); // Strings são cercadas por aspas duplas
 	
 }
 
