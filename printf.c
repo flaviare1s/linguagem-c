1d8226db3b413df59a4a65b84173ce0adf6083e4
Flávia Reis <flaviarster@gmail.com>
2024-04-18 15:03:05 -0300

    Primeiros passos

---
+++ printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	printf("Olá, tenho 41 anos e hoje estou começando a aprender a programar em C.\n");
+	
+	printf("Valor inteiro: %d.\n", 10);
+	printf("Valor decimal: %f.\n", 3.141516171819);
+	printf("Valor decimal com apenas duas casas após a vírgula: %.2f.\n", 3.141516171819);
+	printf("Imprimindo caractere: %c.\n", 'a');
+	printf("Imprimindo texto: %s.\n", "testando");
+	
+}
+
