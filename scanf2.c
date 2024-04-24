3fc41ffa54c70525ac35ce30dd7a70a5372dda75
Flávia Reis <flaviarster@gmail.com>
2024-04-24 09:16:47 -0300

    Scanf2

---
+++ scanf2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	float altura = 0;
+	
+	printf("Valor inicial da altura:%.2f.\n", altura);
+	
+	printf("Qual a sua altura?\n");
+	
+	scanf("%f", &altura);
+	
+	printf("Sua altura é:%.2f.\n", altura);
+}
