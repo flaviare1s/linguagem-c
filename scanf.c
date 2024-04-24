0644f088f697bedbdbe6f4959b09f3892223439c
Flávia Reis <flaviarster@gmail.com>
2024-04-24 08:55:35 -0300

    Criei scanf.c

---
+++ scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int idade = 0;
+	
+	printf("Valor inicial da idade: %d.\n", idade);
+	
+	printf("Digite sua idade:\n");
+	
+	scanf("%d", &idade);
+	
+	printf("Sua idade é: %d.\n", idade);
+}
