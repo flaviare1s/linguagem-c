aaa64dd5c5bdc43d59abc07175701b4fb0b269c0
Flávia Reis <flaviarster@gmail.com>
2024-04-25 11:47:31 -0300

    Lista de exercícios da faculdade

---
+++ Trabalho Faculdade/main.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+
+/*
+int main() {
+	int num;
+	
+	printf("Digite um numero inteiro:\n");
+	
+	scanf("%d", &num);
+	
+	printf("Dobro:%d.\n", num * 2);
+	printf("Triplo:%d.\n", num * 3);
+}
+*/
+
+/*
+int main() {
+	int num1, num2;
+	
+	printf("Digite um numero inteiro:\n");
+	scanf("%d", &num1);
+	
+	printf("Digite outro numero inteiro:\n");
+	scanf("%d", &num2);
+	
+	printf("Soma:%d.\n", num1 + num2);
+	printf("Produto do primeiro pelo quadrado do segundo:%d.\n", num1 * (num2 * num2));
+	printf("Quadrado do primeiro numero:%d.\n", num1 * num1);
+	
+}
+*/
+
+/*
+int main() {
+	float num1, num2;
+	
+	printf("Digite um numero real:\n");
+	scanf("%f", &num1);
+	
+	printf("Digite outro numero real:\n");
+	scanf("%f", &num2);
+	
+	printf("Media:%.2f.\n", (num1 + num2) / 2);
+	
+}
+*/
+
+/*
+int main() {
+	int num1, num2;
+	
+	printf("Digite um numero inteiro:\n");
+	scanf("%d", &num1);
+	
+	printf("Digite outro numero inteiro:\n");
+	scanf("%d", &num2);
+	
+	printf("Media:%d.\n", num1 % num2);
+	
+}
+*/
+
+/*
+int main() {
+	int num1, num2;
+	
+	printf("Digite um numero inteiro:\n");
+	scanf("%d", &num1);
+	
+	printf("Digite um numero inteiro:\n");
+	scanf("%d", &num2);
+	
+	printf("Dobro do primeiro:%d.\n", num1 * 2);
+	printf("Triplo do primeiro:%d.\n", num1 * 3);
+	
+	printf("Dobro do segundo:%d.\n", num2 * 2);
+	printf("Triplo do segundo:%d.\n", num2 * 3);
+}
+*/
+
+/*
+int main() {
+	int idade;
+	
+	printf("Digite sua idade:\n");
+	
+	scanf("%d", &idade);
+	
+	printf("Voce tem %d anos!\n", idade);
+}
+*/
+
+/*
+int main() {
+	int anoNasc, anoAtual;
+	
+	printf("Digite o ano que voce nasceu:\n");
+	scanf("%d", &anoNasc);
+	
+	printf("Digite o ano atual:\n");
+	scanf("%d", &anoAtual);
+	
+	printf("Sua idade eh %d anos.\n", anoAtual - anoNasc);
+}
+*/
+
+/*
+int main() {
+	float temperaturaCelsius;
+	
+	printf("Digite a temperatura em Celsius:\n");
+	scanf("%f", &temperaturaCelsius);
+	
+	printf("Temperatura em Fahrenheit:%.2f.\n", 180 * (temperaturaCelsius + 32) / 100);
+	
+}
+*/
+
+/*
+int main() {
+	float peso;
+	
+	printf("Digite seu peso:\n");
+	scanf("%f", &peso);
+	
+	printf("Peso se engordar 15%%: %.2f.\n", peso + (peso * 0.15));
+	printf("Peso se emagrecer 20%%: %.2f.\n", peso - (peso * 0.2));
+	
+}
+*/
+
+int main() {
+	float nota1, nota2;
+	
+	printf("Digite a primeira nota:\n");
+	scanf("%f", &nota1);
+	printf("Digite a segunda nota:\n");
+	scanf("%f", &nota2);
+	
+	printf("Media ponderada:%.2f.\n", ((nota1 * 2) + (nota2 *3)) / 5);
+}
+
+
+
+
+	
